075e5ee4e75bd1aaa11b97e6ddcbeda274ec132f
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-02-26 15:31:39 -0500

    Score Counter

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = (int)Time.time;
+        scoreText.text = "Score: "  + score.ToString();
+    }
+}
