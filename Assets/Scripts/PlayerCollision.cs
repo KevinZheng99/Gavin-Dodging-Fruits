b3257ec9176d09c9604e3e98e2df254420978e6f
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-03-18 20:49:11 -0400

    Heart Container

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public List<GameObject> hearts;
     void OnCollisionEnter2D (Collision2D collisionInfo) {
         if (collisionInfo.collider.tag == "Fruit") {
             Destroy(collisionInfo.collider.gameObject, 0f);
